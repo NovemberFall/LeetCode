9dad4440220b620b2bc7cb761dba1efe75d25a32
november <1219.zheng@gmail.com>
2022-07-01 00:58:38 -0700

    up

--- workplace/src/ObjectOrientedDesign/MaxStack/MaxStack.java
+++ workplace/src/ObjectOrientedDesign/MaxStack/MaxStack.java
@@ -1,10 +1,8 @@
 package ObjectOrientedDesign.MaxStack;
 
 import java.util.ArrayDeque;
-import java.util.Arrays;
 import java.util.Deque;
 import java.util.PriorityQueue;
-import java.util.Stack;
 
 class MaxStack {
 
