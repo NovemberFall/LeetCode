445de6c21d2d8cbca9dd5a8976c80e9fe2fb3cea
november <1219.zheng@gmail.com>
2022-07-03 00:51:36 -0700

    up

---
+++ workplace/src/ObjectOrientedDesign/Calculator/calculator.java
@@ -0,0 +1,42 @@
+package ObjectOrientedDesign.Calculator;
+
+import java.util.Stack;
+
+public class calculator {
+    public static int calculate(String s) {
+        if (s == null) return 0;
+
+        int result = 0;
+        int sign = 1;
+        int num = 0;
+
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(sign);
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c >= '0' && c <= '9') {
+                num = num * 10 + (c - '0');
+
+            } else if (c == '+') {
+                result += sign * num;
+                sign = stack.peek() * 1;
+                num = 0;
+
+            } else if (c == '-') {
+                result += sign * num;
+                sign = stack.peek() * -1;
+                num = 0;
+            } else if (c == '(') {
+                stack.push(sign);
+
+            } else if (c == ')') {
+                stack.pop();
+            }
+        }
+
+        result += sign * num;
+        return result;
+    }
+}
