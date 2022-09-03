9bb4604b70521e48875d9723649fdaeea29bd985
TomZheng <1219.zheng@gmail.com>
2022-09-02 23:49:39 -0700

    up

--- workplace/src/ObjectOrientedDesign/DesignHitCounter/_362_DesignHitCounter.java
+++ workplace/src/ObjectOrientedDesign/DesignHitCounter/_362_DesignHitCounter.java
@@ -1,5 +1,27 @@
 package ObjectOrientedDesign.DesignHitCounter;
 
-public class _362_DesignHitCounter {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class _362_DesignHitCounter {
+    Queue<Integer> queue;
+
+    public _362_DesignHitCounter() {
+        queue = new LinkedList<>();
+    }
+
+    /** Record a hit.
+     @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        queue.offer(timestamp);
+    }
+
+    /** Return the number of hits in the past 5 minutes.
+     @param timestamp - The current timestamp (in seconds granularity). */
+    public int get(int timestamp) {
+        while (!queue.isEmpty() && timestamp - queue.peek() >= 300) {
+            queue.poll();
+        }
+        return queue.size();
+    }
 }
