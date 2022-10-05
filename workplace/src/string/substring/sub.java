fb671874675482f58e937868b651cf947c560d14
TomZheng <1219.zheng@gmail.com>
2022-10-05 15:25:52 -0700

    up

---
+++ workplace/src/string/substring/sub.java
@@ -0,0 +1,8 @@
+package string.substring;
+
+public class sub {
+    public static void main(String[] args) {
+        String str = "helloworld";
+        System.out.println(str.substring(0, 4));
+    }
+}
---
+++ workplace/src/string/substring/test1.java
@@ -0,0 +1,8 @@
+package string.substring;
+
+public class test1 {
+    public static void main(String[] args) {
+        String str = "hello";
+        System.out.println(str.substring(1));
+    }
+}
