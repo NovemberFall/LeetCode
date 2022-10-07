4a0407b27437bf9f63427417cef87550b8425532
november <1219.zheng@gmail.com>
2022-10-07 00:42:54 -0700

    update

---
+++ workplace/src/ObjectOrientedDesign/HashMap_Implementation/Main.java
@@ -0,0 +1,10 @@
+package ObjectOrientedDesign.HashMap_Implementation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Main {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+    }
+}
