c18e857f07cddf491a3f07dbec3859a6bc01cc9c
TomZheng <1219.zheng@gmail.com>
2022-10-07 11:26:09 -0700

    up

--- workplace/src/ObjectOrientedDesign/HashMap_Implementation/MyHashMap.java
+++ workplace/src/ObjectOrientedDesign/HashMap_Implementation/MyHashMap.java
@@ -3,7 +3,7 @@ package ObjectOrientedDesign.HashMap_Implementation;
 import java.util.HashMap;
 import java.util.Map;
 
-class Main {
+class MyHashMap<K, V> {
     public static void main(String[] args) {
         Map<String, Integer> map = new HashMap<>();
     }
