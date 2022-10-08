09a797737097de81ba0a5d186be70a5324949da5
TomZheng <1219.zheng@gmail.com>
2022-10-08 00:32:13 -0700

    update

---
+++ workplace/src/ObjectOrientedDesign/HashMap_Implementation/Test.java
@@ -0,0 +1,11 @@
+package ObjectOrientedDesign.HashMap_Implementation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Test {
+    public static void main(String[] args) {
+        System.out.println(0.75f);
+        Map<String, String> map = new HashMap<>();
+    }
+}
