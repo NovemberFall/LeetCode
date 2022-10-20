24666cf95ecd3bec62ffcc311f61747a5d6823c5
november <1219.zheng@gmail.com>
2022-10-19 21:48:22 -0700

    update

---
+++ leetCode/src/main/java/BitOperation/Print_Num/Solution.java
@@ -0,0 +1,17 @@
+package BitOperation.Print_Num;
+
+class Solution {
+
+    private static void print(int num) {
+        String ans = "";
+        for (int i = 31; i >= 0; i--) {
+            ans = (num & (1 << i)) == 0 ? "0" : "1";
+            System.out.print(ans + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(1 << 3);
+        print(8);
+    }
+}
