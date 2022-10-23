9010b9fc92a9638a32b4f5ad9391f76f7fbf840e
TomZheng <1219.zheng@gmail.com>
2022-10-22 21:30:40 -0700

    update

--- leetCode/src/main/java/string/String_Pool/StringPool.java
+++ leetCode/src/main/java/string/String_Pool/StringPool.java
@@ -3,10 +3,33 @@ package string.String_Pool;
 import org.junit.jupiter.api.Test;
 
 public class StringPool {
+
+    /*
+    Pooling for String objects
+    There is no need for
+     */
     @Test
-    public void testAssertThat() {
+    public void string_Interning() {
         String constantString1 = "string";
         String constantString2 = "string";
-        assertEquals();
+        System.out.println(constantString1 == constantString2);
     }
+
+
+    /*
+       When we create a String via the new operator, the Java compiler will create a new object and
+       store it in the heap space reserved for the JVM.
+
+       Every String created like this will point to a different memory region with its own address.
+       Let’s see how this is different from the previous case:
+     */
+    @Test
+    void strings_Allocated_Using_Constructor() {
+        String constantString = "gradle";
+        String newString = new String("gradle");
+        System.out.println(constantString == newString);
+    }
+
+
+
 }
