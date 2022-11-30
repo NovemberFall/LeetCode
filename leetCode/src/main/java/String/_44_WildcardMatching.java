42a827d77b8307f93498659e8f275bc51ee330a2
TomZheng <1219.zheng@gmail.com>
2022-11-30 13:25:34 -0800

    LC 44

--- leetCode/src/main/java/String/_44_WildcardMatching.java
+++ leetCode/src/main/java/String/_44_WildcardMatching.java
@@ -1,25 +1,25 @@
 package String;
 
 class _44_WildcardMatching {
-    public boolean isMatch(String str, String pattern) {
-        int s = 0, p = 0, match = 0, starIdx = -1;
-        while (s < str.length()){
+    public boolean isMatch(String s, String p) {
+        int sp = 0, pp = 0, match = 0, starIdx = -1;
+        while (sp < s.length()){
             // advancing both pointers
-            if (p < pattern.length()  && (pattern.charAt(p) == '?' || str.charAt(s) == pattern.charAt(p))){
-                s++;
-                p++;
+            if (pp < p.length()  && (p.charAt(pp) == '?' || s.charAt(sp) == p.charAt(pp))){
+                sp++;
+                pp++;
             }
             // * found, only advancing pattern pointer
-            else if (p < pattern.length() && pattern.charAt(p) == '*'){
-                starIdx = p;
-                match = s;
-                p++;
+            else if (pp < p.length() && p.charAt(pp) == '*'){
+                starIdx = pp;
+                match = sp;
+                pp++;
             }
             // last pattern pointer was *, advancing string pointer
             else if (starIdx != -1){
-                p = starIdx + 1;
+                pp = starIdx + 1;
                 match++;
-                s = match;
+                sp = match;
             }
             //current pattern pointer is not star, last patter pointer was not *
             //characters do not match
@@ -29,10 +29,17 @@ class _44_WildcardMatching {
         }
 
         //check for remaining characters in pattern
-        while (p < pattern.length() && pattern.charAt(p) == '*') {
-            p++;
+        while (pp < p.length() && p.charAt(pp) == '*') {
+            pp++;
         }
 
-        return p == pattern.length();
+        return pp == p.length();
+    }
+
+    public static void main(String[] args) {
+        _44_WildcardMatching wm = new _44_WildcardMatching();
+        String s = "acdcb", p = "a*c?b";
+        boolean res = wm.isMatch(s, p);
+        System.out.println(res);
     }
 }
