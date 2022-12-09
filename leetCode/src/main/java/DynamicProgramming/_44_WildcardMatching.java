259af10b9c1d318072108bebc51a358e3a2bfc1c
TomZheng <1219.zheng@gmail.com>
2022-12-09 00:41:30 -0800

    LC 10

---
+++ leetCode/src/main/java/DynamicProgramming/_44_WildcardMatching.java
@@ -0,0 +1,35 @@
+package DynamicProgramming;
+
+class _44_WildcardMatching {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int j = 1; j < dp[0].length; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (p.charAt(j - 1) == s.charAt(i - 1) || p.charAt(j - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        _44_WildcardMatching wm = new _44_WildcardMatching();
+        String s = "acdcb", p = "a*c?b";
+        boolean res = wm.isMatch(s, p);
+        System.out.println(res);
+    }
+}
