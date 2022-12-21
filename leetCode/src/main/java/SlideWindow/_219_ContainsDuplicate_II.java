dc1e91dcbd3a057e2db3611530addbf765dc8ce0
TomZheng <1219.zheng@gmail.com>
2022-12-20 18:06:50 -0800

    lc 219

--- leetCode/src/main/java/SlideWindow/_219_ContainsDuplicate_II.java
+++ leetCode/src/main/java/SlideWindow/_219_ContainsDuplicate_II.java
@@ -1,7 +1,36 @@
 package SlideWindow;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 class _219_ContainsDuplicate_II {
+    public boolean containsNearbyDuplicate_Self(int[] nums, int k) {
+        if (nums == null || nums.length == 0) return true;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && Math.abs(i - map.get(nums[i])) <= k) {
+                return true;
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (nums == null || nums.length == 0) return true;
+
+        Set<Integer> set = new HashSet<>();
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (!set.add(nums[fast])) {
+                return true;
+            }
+            if (fast >= k) {
+                set.remove(nums[fast - k]);
+            }
+        }
         return false;
     }
 }
