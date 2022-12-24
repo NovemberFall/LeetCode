779767ba7cdeb834b7b1700807b5228d704b26c0
TomZheng <1219.zheng@gmail.com>
2022-12-23 22:07:31 -0800

    LC 76

--- leetCode/src/main/java/JavaAPI/hashMapAPI/getOrDefault.java
+++ leetCode/src/main/java/JavaAPI/hashMapAPI/getOrDefault.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.Test;
 
 class getOrDefault {
     public static void main(String[] args) {
@@ -17,5 +18,18 @@ class getOrDefault {
 //        System.out.println(map.get(str));
         List<Integer> l2 = map.getOrDefault(str, new ArrayList<>());
         System.out.println(l2);
+    }
+
+    @Test
+    void test_getOrDefault() {
+        Map<Character, Integer> map = new HashMap<>();
+        String str = "ADOBECODEBANC";
+        for (char c : str.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
     }
 }
---
+++ leetCode/src/main/java/SlideWindow/_239_SlidingWindowMaximum.java
@@ -0,0 +1,67 @@
+package SlideWindow;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+
+/*
+k = 3
+          0  1   2   3  4  5  6  7
+         [1, 3, -1, -3, 5, 3, 6, 7]
+    DQ:   0
+    DQ:      1
+    DQ:      1   2                          List[3, ]
+    DQ:      1   2   3                      List[3, 3]
+    DQ:                                     List[3, 3]   # poll() <== 1, 2, 3
+    DQ:                 4                   List[3, 3, 5]
+    DQ:                 4  5                List[3, 3, 5, 5]
+    DQ:                       6             List[3, 3, 5, 5]  # poll() <== 4, 5
+    DQ:                       6             List[3, 3, 5, 5, 6]
+    DQ:                          7           List[3, 3, 5, 5, 6, 7]
+
+ */
+class _239_SlidingWindowMaximum {
+    public static int[] maxSlidingWindow_BruteForce(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k == 0) return new int[0];
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = k - 1; i < nums.length; i++) {
+            int curMax = nums[i];
+            for (int j = 0; j < k; j++) {
+                curMax = Math.max(curMax, nums[i - j]);
+            }
+            res.add(curMax);
+        }
+        return res.stream().mapToInt(i -> i).toArray();
+    }
+
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k == 0) return new int[0];
+
+        List<Integer> res = new ArrayList<>();
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 0; i < nums.length; i++) {
+            while (!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]) {
+                dq.pollLast();
+            }
+            while (!dq.isEmpty() && dq.peekFirst() <= i - k) {
+                dq.pollFirst();
+            }
+
+            dq.offerLast(i);
+
+            if (i + 1 >= k) {
+                res.add(nums[dq.peekFirst()]);
+            }
+        }
+        return res.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 3, -1, -3, 5, 3, 6, 7};
+        int[] res = maxSlidingWindow_BruteForce(nums, 3);
+        System.out.println(Arrays.toString(res));
+    }
+}
