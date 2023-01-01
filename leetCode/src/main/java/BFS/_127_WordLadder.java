addb31e48d0fd7d5a374a13c8bb857eef158b333
TomZheng <1219.zheng@gmail.com>
2023-01-01 10:58:10 -0800

    LC 127

--- leetCode/src/main/java/BFS/_127_WordLadder.java
+++ leetCode/src/main/java/BFS/_127_WordLadder.java
@@ -1,9 +1,50 @@
 package BFS;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
 
 class _127_WordLadder {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> dict = new HashSet<>(wordList);
+        if (dict.isEmpty() || !dict.contains(endWord)) {
+            return 0;
+        }
+        Queue<String> queue = new ArrayDeque<>();
+        queue.offer(beginWord);
+        int level = 1;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String curWord = queue.poll();
+                if (curWord.equals(endWord)) {
+                    return level;
+                }
+                for (char c = 'a'; c <= 'z'; c++) {
+                    for (int j = 0; j < curWord.length(); j++) {
+                        String newWord =
+                                curWord.substring(0, j) + c + curWord.substring(j + 1);
+                        if (dict.contains(newWord)) {
+                            queue.offer(newWord);
+                            dict.remove(newWord);
+                        }
+                    }
+                }
+            }
+            level++;
+        }
         return 0;
+    }
+
+    public static void main(String[] args) {
+        _127_WordLadder wordLadder = new _127_WordLadder();
+        List<String> wordList = Arrays.asList("hot", "dot", "dog", "lot", "log", "cog");
+        String beginWord = "hit", endWord = "cog";
+        int res = wordLadder.ladderLength(beginWord, endWord, wordList);
+        System.out.println(res);
     }
 }
