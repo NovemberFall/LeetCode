4ac5f72830a632a30ceada501dfd966f5baa0776
TomZheng <1219.zheng@gmail.com>
2023-01-01 01:26:43 -0800

    LC 127

---
+++ leetCode/src/main/java/BFS/_116_PopulatingNextRightPointersInEachNode.java
@@ -0,0 +1,66 @@
+package BFS;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+class _116_PopulatingNextRightPointersInEachNode {
+    static class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    public Node connect(Node root) {
+        if (root == null) return root;
+
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node cur = queue.poll();
+                if (i < size - 1) {
+                    cur.next = queue.peek();
+                }
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        _116_PopulatingNextRightPointersInEachNode populatingNextRightPointersInEachNode = new _116_PopulatingNextRightPointersInEachNode();
+        Node head = populatingNextRightPointersInEachNode.connect(root);
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+    }
+}
---
+++ leetCode/src/main/java/BFS/_127_WordLadder.java
@@ -0,0 +1,9 @@
+package BFS;
+
+import java.util.List;
+
+class _127_WordLadder {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        return 0;
+    }
+}
