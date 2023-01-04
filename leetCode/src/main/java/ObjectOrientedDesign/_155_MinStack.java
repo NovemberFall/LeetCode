c4a45cb884b5d3d577be42ff1dceea6f1b09ded9
TomZheng <1219.zheng@gmail.com>
2023-01-03 16:21:59 -0800

    LC 225

---
+++ leetCode/src/main/java/ObjectOrientedDesign/_155_MinStack.java
@@ -0,0 +1,23 @@
+package ObjectOrientedDesign;
+
+class _155_MinStack {
+    public _155_MinStack() {
+
+    }
+
+    public void push(int val) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+
+    }
+
+    public int getMin() {
+
+    }
+}
