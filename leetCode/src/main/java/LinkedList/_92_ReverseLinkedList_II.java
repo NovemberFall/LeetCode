8106f81a3cbc183bd7ba781671fe51e2106dd3cf
TomZheng <1219.zheng@gmail.com>
2023-01-09 01:00:00 -0800

    83

--- leetCode/src/main/java/LinkedList/_92_ReverseLinkedList_II.java
+++ leetCode/src/main/java/LinkedList/_92_ReverseLinkedList_II.java
@@ -7,13 +7,29 @@ public class _92_ReverseLinkedList_II {
         }
         ListNode dummy = new ListNode(9527);
         dummy.next = head;
-        ListNode cur = head;
-        int count = 1;
-        while (count < left) {
-            cur = cur.next;
-            count++;
-        }
-        head.next = null;
 
+        // 1) reach node at position "left"
+        ListNode leftPrev = dummy;
+        ListNode cur = head;
+        for (int i = 0; i < left - 1; i++) {
+            leftPrev = cur;
+            cur = cur.next;
+        }
+
+        // Now cur = "left", leftPrev = "node before left"
+        // 2) reverse from left to right
+        ListNode prev = null;
+        ListNode next = null;
+        for (int j = 0; j < right - left + 1; j++) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+
+        leftPrev.next.next = cur;
+        leftPrev.next = prev;
+
+        return dummy.next;
     }
 }
