c0cf6afd95180b131581402cb5331e983397dff4
TomZheng <1219.zheng@gmail.com>
2023-01-26 22:04:08 -0800

    LC 46

--- leetCode/src/main/java/DFS/_46_Permutations/Permutations_Final.java
+++ leetCode/src/main/java/DFS/_46_Permutations/Permutations_Final.java
@@ -2,29 +2,40 @@ package DFS._46_Permutations;
 
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.Test;
 
+/*
+                               [1,2,3]
+                 1/               |2                 \3
+                [1]              [2]                 [3]
+          2/       3\         1/     3\           1/     2\
+       [1,2]     [1,3]      [2,1]    [2,3]       [3,1]    [3,2]
+         3|        2|         3|        1|        2|         1|
+       [1,2,3]   [1,3,2]    [2,1,3]  [2,3,1]     [3,1,2]    [3,2,1]
+ */
 class Permutations_Final {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         if (nums == null || nums.length == 0) return res;
 
-        dfs(res, new ArrayList<>(), nums);
+        boolean[] visited = new boolean[nums.length];
+        dfs(res, new ArrayList<>(), nums, visited);
         return res;
     }
 
-    private void dfs(List<List<Integer>> res, List<Integer> path, int[] nums) {
+    private void dfs(List<List<Integer>> res, List<Integer> path, int[] nums, boolean[] visited) {
         if (path.size() == nums.length) {
             res.add(new ArrayList<>(path));
             return;
         }
 
         for (int i = 0; i < nums.length; i++) {
-            if (path.contains(nums[i])) {
+            if (visited[i]) {
                 continue;
             }
             path.add(nums[i]);
-            dfs(res, path, nums);
+            visited[i] = true;
+            dfs(res, path, nums, visited);
+            visited[i] = false;
             path.remove(path.size() - 1);
         }
     }
--- leetCode/src/main/java/DFS/_46_Permutations/Permutations_v3.java
+++ leetCode/src/main/java/DFS/_46_Permutations/Permutations_v3.java
@@ -3,7 +3,16 @@ package DFS._46_Permutations;
 import java.util.ArrayList;
 import java.util.List;
 
-class Permutations {
+/*
+                               [1,2,3]
+                 1/               |2                 \3
+                [1]              [2]                 [3]
+          2/       3\         1/     3\           1/     2\
+       [1,2]     [1,3]      [2,1]    [2,3]       [3,1]    [3,2]
+         3|        2|         3|        1|        2|         1|
+       [1,2,3]   [1,3,2]    [2,1,3]  [2,3,1]     [3,1,2]    [3,2,1]
+ */
+class Permutations_v3 {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         if (nums == null || nums.length == 0) return res;
@@ -19,6 +28,9 @@ class Permutations {
         }
 
         for (int i = 0; i < nums.length; i++) {
+            if (path.contains(nums[i])) {
+                continue;
+            }
             path.add(nums[i]);
             dfs(res, path, nums);
             path.remove(path.size() - 1);
@@ -27,8 +39,8 @@ class Permutations {
 
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3};
-        Permutations pv1 = new Permutations();
-        List<List<Integer>> res = pv1.permute(nums);
+        Permutations_v3 pvf = new Permutations_v3();
+        List<List<Integer>> res = pvf.permute(nums);
         System.out.println(res);
     }
 }
