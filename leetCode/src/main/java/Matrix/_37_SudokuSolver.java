1d0510b07476a8ea9a7261253160bd5b936ecb37
TomZheng <1219.zheng@gmail.com>
2023-01-30 22:40:44 -0800

    LC 36

--- leetCode/src/main/java/Matrix/_37_SudokuSolver.java
+++ leetCode/src/main/java/Matrix/_37_SudokuSolver.java
@@ -1,5 +1,7 @@
 package Matrix;
 
+import java.util.Arrays;
+
 class _37_SudokuSolver {
     public void solveSudoku(char[][] board) {
         solve(board);
@@ -9,16 +11,21 @@ class _37_SudokuSolver {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] == '.') {
+                    // We want to try every number for this block
                     for (char num = '1'; num <= '9'; num++) {
                         if (isValid(board, i, j, num)) {
-                            board[i][j] = num;
+                            board[i][j] = num;// Set the value of the current square to the valid num
                             if (solve(board)) {
+                                //if it's true that means we have a solved board, so you just keep returning
                                 return true;
                             }
                             board[i][j] = '.';
+                            //If our board isn't solved, backtrack and try the next number
                         }
                     }
                     return false;
+                    // If you get to this step, that means that no values fit, which means the current iteration of the board is wrong
+                    // so return false and try the previous step again with a different value
                 }
             }
         }
@@ -40,5 +47,20 @@ class _37_SudokuSolver {
         return true;
     }
 
-
+    public static void main(String[] args) {
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        _37_SudokuSolver sudokuSolver = new _37_SudokuSolver();
+        sudokuSolver.solveSudoku(board);
+//        System.out.println(Arrays.deepToString(board));
+    }
 }
