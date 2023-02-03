303c7701ce0f3df70488e5781a00c6a59a215b10
TomZheng <1219.zheng@gmail.com>
2023-02-03 00:27:19 -0800

    lc 93

--- leetCode/src/main/java/DFS/_93_RestoreIpAddress/Restore_IP_Addresses.java
+++ leetCode/src/main/java/DFS/_93_RestoreIpAddress/Restore_IP_Addresses.java
@@ -3,27 +3,27 @@ package DFS._93_RestoreIpAddress;
 import java.util.ArrayList;
 import java.util.List;
 
-//class Restore_IP_Addresses {
-//    public List<String> restoreIpAddresses(String s) {
-//        List<String> res = new ArrayList<>();
-//        if (s.length() < 4 || s.length() > 12) {
-//            return res;
-//        }
-//
-//        dfs(res, s, 0, 0);
-//        return res;
-//    }
-//
-//    private void dfs(List<String> res, String s, int startIndex, int pointNum) {
-//        if (pointNum == 3) {
-//            if (isValid(s, startIndex, s.length() - 1)) {
-//                res.add(s);
-//            }
-//            return;
-//        }
-//
-//        for (int i = startIndex; i < s.length(); i++) {
-//
-//        }
-//    }
-//}
+class Restore_IP_Addresses {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> res = new ArrayList<>();
+        if (s.length() < 4 || s.length() > 12) {
+            return res;
+        }
+
+        dfs(res, s, 0, 0);
+        return res;
+    }
+
+    private void dfs(List<String> res, String s, int startIndex, int pointNum) {
+        if (pointNum == 3) {
+            if (isValid(s, startIndex, s.length() - 1)) {
+                res.add(s);
+            }
+            return;
+        }
+
+        for (int i = startIndex; i < s.length(); i++) {
+
+        }
+    }
+}
