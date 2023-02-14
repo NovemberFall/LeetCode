1289797347dd718ba8481a0162cfb6b752457a63
TomZheng <1219.zheng@gmail.com>
2023-02-13 17:59:33 -0800

    update

--- leetCode/src/main/java/Monotonic_Stack/_84_LargestRectangleInHistogram/LargestRectangleHistogram.java
+++ leetCode/src/main/java/Monotonic_Stack/_84_LargestRectangleInHistogram/LargestRectangleHistogram.java
@@ -2,6 +2,19 @@ package Monotonic_Stack._84_LargestRectangleInHistogram;
 
 import java.util.Stack;
 
+/*
+1. Create an empty stack.
+2. Start from the first bar, and do the following for every bar hist[i] where ‘i‘ varies from 0 to n-1
+    1. If the stack is empty or hist[i] is higher than the bar at top of the stack, then push ‘i‘ to stack.
+    2. If this bar is smaller than the top of the stack, then keep removing the top of the stack while
+        the top of the stack is greater.
+    3. Let the removed bar be hist[tp]. Calculate the area of the rectangle with hist[tp] as the smallest bar.
+    4. For hist[tp], the ‘left index’ is previous (previous to tp) item in stack
+        and ‘right index’ is ‘i‘ (current index).
+3. If the stack is not empty, then one by one remove all bars from the stack and
+    do step (2.2 and 2.3) for every removed bar
+
+ */
 class LargestRectangleHistogram {
     public int largestRectangleArea(int[] heights) {
         Stack<Integer> stk = new Stack<>();
