b4a70c8f9073fb1b12e7111f7e4e41ac1c8b7c6f
TomZheng <1219.zheng@gmail.com>
2023-03-15 13:39:19 -0700

    lc 41

--- leetCode/src/main/java/HashTable/_41_FirstMissingPositive/FirstMissingPositive.java
+++ leetCode/src/main/java/HashTable/_41_FirstMissingPositive/FirstMissingPositive.java
@@ -1,5 +1,28 @@
 package HashTable._41_FirstMissingPositive;
 
 class FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n
+                    &&
+                    nums[nums[i] - 1] != nums[i]) {
 
+                swap(nums, i, nums[i] - 1);
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
 }
