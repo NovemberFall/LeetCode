eceb98b17776db4458dbd90a0c46dd38e73fe2f7
TomZheng <1219.zheng@gmail.com>
2023-03-21 21:44:44 -0700

    lc 30

--- leetCode/src/main/java/SlideWindow/_30_SubstringWithConcatenationOfAllWords.java
+++ leetCode/src/main/java/SlideWindow/_30_SubstringWithConcatenationOfAllWords.java
@@ -1,5 +1,73 @@
 package SlideWindow;
 
-class _30_SubstringWithConcatenationOfAllWords {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+class _30_SubstringWithConcatenationOfAllWords {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> res = new ArrayList<>();
+        if (words.length == 0 || s.length() < words.length * words[0].length()) {
+            return res;
+        }
+        int n = s.length(), m = words.length;
+        int wordLen = words[0].length();
+        Map<String, Integer> map = buildMap(words);
+        Map<String, Integer> curMap = new HashMap<>();
+        String str = null, tmp = null;
+
+        for (int i = 0; i < wordLen; i++) {
+            int count = 0; // remark: reset count
+            int start = i;
+            for (int r = i; r + wordLen <= n; r += wordLen) {
+                str = s.substring(r, r + wordLen);
+                if (map.containsKey(str)) {
+                    if (curMap.containsKey(str)) {
+                        curMap.put(str, curMap.get(str) + 1);
+                    } else {
+                        curMap.put(str, 1);
+                    }
+
+                    if (curMap.get(str) <= map.get(str)) {
+                        count++;
+                    }
+                    while (curMap.get(str) > map.get(str)) {
+                        tmp = s.substring(start, start + wordLen);
+                        curMap.put(tmp, curMap.get(tmp) - 1);
+                        start += wordLen;
+
+                        if (curMap.get(tmp) < map.get(tmp)) {
+                            count--;
+                        }
+                    }
+                    if (count == m) {
+                        res.add(start);
+                        tmp = s.substring(start, start + wordLen);
+                        curMap.put(tmp, curMap.get(tmp) - 1);
+                        start += wordLen;
+                        count--;
+                    }
+                } else {
+                    curMap.clear();
+                    count = 0;
+                    start = r + wordLen; //not contain, so move the start
+                }
+            }
+            curMap.clear();
+        }
+        return res;
+    }
+
+    private Map<String, Integer> buildMap(String[] words) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            if (map.containsKey(word)) {
+                map.put(word, map.get(word) + 1);
+            } else {
+                map.put(word, 1);
+            }
+        }
+        return map;
+    }
 }
--- leetCode/src/main/java/String/API/substring/sub.java
+++ leetCode/src/main/java/String/API/substring/sub.java
@@ -3,9 +3,8 @@ package String.API.substring;
 public class sub {
     public static void main(String[] args) {
         String str = "0123456789";
-        System.out.println(str.substring(0, 1));
-        System.out.println(str.substring(0, 3));
-        System.out.println(str.substring(0, 9));
-
+        System.out.println(str.substring(0, 1)); // 0
+        System.out.println(str.substring(0, 3)); // 012
+        System.out.println(str.substring(0, 9)); // 012345678
     }
 }
