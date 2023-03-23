37a78809c127e909ac31f993362ec69db42fc205
TomZheng <1219.zheng@gmail.com>
2023-03-23 00:47:11 -0700

    lc 166

---
+++ leetCode/src/main/java/BitOperation/Basic/XOR/test.java
@@ -0,0 +1,10 @@
+package BitOperation.Basic.XOR;
+
+class test {
+    public static void main(String[] args) {
+        System.out.println(-4 > 0);
+        System.out.println(5 > 0);
+        boolean res = (-4 > 0) ^ (5 > 0);
+        System.out.println(res);
+    }
+}
