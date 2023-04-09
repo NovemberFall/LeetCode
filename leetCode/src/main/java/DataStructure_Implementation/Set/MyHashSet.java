e1e3fa5d2d99da8c4ede620acfa376a2350ebe44
TomZheng <1219.zheng@gmail.com>
2023-04-09 16:57:08 -0700

    java TDD

---
+++ leetCode/src/main/java/DataStructure_Implementation/Set/MyHashSet.java
@@ -0,0 +1,88 @@
+package DataStructure_Implementation.Set;
+
+import java.util.HashSet;
+import java.util.Set;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class MyHashSet {
+    private static class MySet<E> {
+        private final Set<E> set;
+
+        private MySet() {
+            this.set = new HashSet<>();
+        }
+
+        public void add(E element) {
+            set.add(element);
+        }
+
+        public boolean remove(E element) {
+            return set.remove(element);
+        }
+
+        public int size() {
+            return set.size();
+        }
+
+        public boolean contains(E element) {
+            return set.contains(element);
+        }
+    }
+
+    @Test
+    void testAdd() {
+        MySet<String> set = new MySet<>();
+        set.add("foo");
+        set.add("bar");
+        set.add("hello");
+        assertEquals(3, set.size());
+        assertTrue(set.contains("foo"));
+        assertTrue(set.contains("bar"));
+        assertTrue(set.contains("hello"));
+    }
+
+    @Test
+    void testRemove() {
+        MySet<Integer> set = new MySet<>();
+        set.add(10);
+        set.add(20);
+        set.add(30);
+        assertTrue(set.remove(10));
+        assertFalse(set.remove(12));
+        assertEquals(2, set.size());
+        assertFalse(set.contains(30));
+        assertTrue(set.contains(20));
+        assertTrue(set.contains(10));
+    }
+
+    @Test
+    void testContians() {
+        MySet<Character> set = new MySet<>();
+        set.add('a');
+        set.add('b');
+        set.add('c');
+        assertTrue(set.contains('a'));
+        assertTrue(set.contains('b'));
+        assertFalse(set.contains('h'));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- leetCode/src/main/java/Java8/Examples/Functional_Interface_Examples/test.java
+++ leetCode/src/main/java/Java8/Examples/Functional_Interface_Examples/test.java
@@ -1,10 +1,29 @@
 package Java8.Examples.Functional_Interface_Examples;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class test {
     @Test
     void testPredicate() {
+        Predicate<Integer> isEven = x -> x % 2 == 0;
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> evenNumbers = numbers.stream()
+                .filter(isEven)
+                .collect(Collectors.toList());
+        System.out.println(evenNumbers); // [2, 4, 6]
+    }
 
+
+    @Test
+    void testCosumer() {
+        Consumer<String> printString = System.out::println;
+        List<String> strings = Arrays.asList("Hello", "World");
+        strings.forEach(printString); // Hello
+                                      // World
     }
 }
