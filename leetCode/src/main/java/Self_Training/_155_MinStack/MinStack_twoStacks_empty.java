f1f219523ded266acd13c572d20b9ef286d19b74
TomZheng <1219.zheng@gmail.com>
2023-04-27 00:14:11 -0700

    lc 155

---
+++ leetCode/src/main/java/Self_Training/_155_MinStack/MinStack_twoStacks_empty.java
@@ -0,0 +1,31 @@
+package Self_Training._155_MinStack;
+
+import java.util.Stack;
+
+class MinStack_twoStacks_empty {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack_twoStacks_empty() {
+
+    }
+
+    public void push(int val) {
+
+        // when value <= current min value in stack,
+        // need to push the value to minstack
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+        return -1;
+    }
+
+    public int getMin() {
+        return -1;
+    }
+}
