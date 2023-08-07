a44f100258c82a512d02cdce01b0989acaa3bcb8
TomZheng <1219.zheng@gmail.com>
2023-08-07 11:58:22 -0700

    lc 39

--- leetCode/src/main/java/String/API/StringBuilder/Test.java
+++ leetCode/src/main/java/String/API/StringBuilder/Test.java
@@ -4,8 +4,12 @@ class Test {
     public static void main(String[] args) {
         StringBuilder sb = new StringBuilder();
         sb.append("hello");
-        sb = new StringBuilder();
-        sb.append("World");
         System.out.println(sb);
+
+        sb.deleteCharAt(0);
+        System.out.println(sb);
+
+        sb = new StringBuilder("0123456789");
+        System.out.println(sb.charAt(3));
     }
 }
