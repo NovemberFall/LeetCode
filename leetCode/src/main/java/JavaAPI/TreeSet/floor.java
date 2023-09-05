501a5e7e58fb7250aade44ccf802c7a2a051d12e
TomZheng <1219.zheng@gmail.com>
2023-09-04 22:41:45 -0700

    lc 220

---
+++ leetCode/src/main/java/JavaAPI/TreeSet/floor.java
@@ -0,0 +1,22 @@
+package JavaAPI.TreeSet;
+
+import java.util.TreeSet;
+
+class floor {
+    public static void main(String[] args) {
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        treeSet.add(1);
+        treeSet.add(2);
+        treeSet.add(3);
+        treeSet.add(4);
+        treeSet.add(9);
+        treeSet.add(11);
+        treeSet.add(15);
+        treeSet.add(20);
+
+        Integer element = 10;
+        Integer floor = treeSet.floor(element);
+
+        System.out.println("The floor of " + element + " is " + floor);
+    }
+}
--- leetCode/src/main/java/SlideWindow/_219_ContainsDuplicate_II/containsDuplicate_II_slidingWindow.java
+++ leetCode/src/main/java/SlideWindow/_219_ContainsDuplicate_II/containsDuplicate_II_slidingWindow.java
@@ -1,24 +1,11 @@
-package SlideWindow;
+package SlideWindow._219_ContainsDuplicate_II;
 
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-class _219_ContainsDuplicate_II {
-    public boolean containsNearbyDuplicate_Self(int[] nums, int k) {
-        if (nums == null || nums.length == 0) return true;
-
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i]) && Math.abs(i - map.get(nums[i])) <= k) {
-                return true;
-            }
-            map.put(nums[i], i);
-        }
-        return false;
-    }
-
+class containsDuplicate_II_slidingWindow {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         if (nums == null || nums.length == 0) return true;
 
