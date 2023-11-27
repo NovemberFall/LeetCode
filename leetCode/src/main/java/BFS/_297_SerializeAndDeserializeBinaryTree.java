e4abc23f6fe5928889211e0296b464b8535b5072
NovemberFall <1219.zheng@gmail.com>
2023-11-27 17:21:24 +0800

    lc 297

--- leetCode/src/main/java/BFS/_297_SerializeAndDeserializeBinaryTree.java
+++ leetCode/src/main/java/BFS/_297_SerializeAndDeserializeBinaryTree.java
@@ -1,16 +1,18 @@
 package BFS;
 
 import java.util.ArrayDeque;
+import java.util.LinkedList;
 import java.util.Queue;
 
 class _297_SerializeAndDeserializeBinaryTree {
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        // corner case
-        if (root == null) return "";
-
-        //BFS-QUEUE
-        Queue<TreeNode> queue = new ArrayDeque<>();
+        //corner case
+        if (root == null) {
+            return "";
+        }
+        //BFS-Queue;
+        Queue<TreeNode> queue = new LinkedList<>();
         StringBuilder sb = new StringBuilder();
         queue.offer(root);
 
@@ -25,22 +27,23 @@ class _297_SerializeAndDeserializeBinaryTree {
             }
             sb.append(",");
         }
+        // sb.deleteCharAt(sb.length() - 1);
         return sb.toString();
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
+        //corner case
         if (data == "") {
             return null;
         }
-
         //split string by ","
         String[] arr = data.split(",");
-
-        Queue<TreeNode> queue = new ArrayDeque<>();
+        //Queue, not empty
+        Queue<TreeNode> queue = new LinkedList<>();
         TreeNode root = new TreeNode(Integer.parseInt(arr[0]));
         queue.offer(root);
-
+        //1.create treenode; 2. connect to parent node 3. add to queue
         for (int i = 1; i < arr.length; i++) {
             TreeNode cur = queue.poll();
             if (!arr[i].equals("null")) {
@@ -48,6 +51,7 @@ class _297_SerializeAndDeserializeBinaryTree {
                 cur.left = left;
                 queue.offer(left);
             }
+
             if (!arr[++i].equals("null")) {
                 TreeNode right = new TreeNode(Integer.parseInt(arr[i]));
                 cur.right = right;
