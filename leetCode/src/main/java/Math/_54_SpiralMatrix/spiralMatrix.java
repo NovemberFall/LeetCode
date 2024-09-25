ce3854087bf494a2597d85cbf82e1a08a9e219d9
TomZheng <1219.zheng@gmail.com>
2024-09-25 10:19:14 -0700

    lc 54

--- leetCode/src/main/java/Math/_54_SpiralMatrix/spiralMatrix.java
+++ leetCode/src/main/java/Math/_54_SpiralMatrix/spiralMatrix.java
@@ -4,53 +4,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 class spiralMatrix {
-    class Solution {
-        public List<Integer> spiralOrder(int[][] matrix) {
-            List<Integer> res = new ArrayList<>();
-            if (matrix == null || matrix.length == 0) {
-                return res;
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (matrix.length == 0 || matrix[0].length == 0) return res;
+
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+
+        while (true) { // top <= bottom && left <= right
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+            if (left > right || top > bottom) {
+                break;
             }
 
-            int top = 0;
-            int bottom = matrix.length - 1;
-            int left = 0;
-            int right = matrix[0].length - 1;
 
-            while (top <= bottom && left <= right) {
-                // Traverse from left to right along the top row
-                if (top <= bottom) {
-                    for (int i = left; i <= right; i++) {
-                        res.add(matrix[top][i]);
-                    }
-                }
-                top++;  // Move the top boundary down
-
-                // Traverse from top to bottom along the right column
-                if (left <= right) {
-                    for (int i = top; i <= bottom; i++) {
-                        res.add(matrix[i][right]);
-                    }
-                }
-                right--;  // Move the right boundary left
-
-                // Traverse from right to left along the bottom row
-                if (top <= bottom) {
-                    for (int i = right; i >= left; i--) {
-                        res.add(matrix[bottom][i]);
-                    }
-                }
-                bottom--;  // Move the bottom boundary up
-
-                // Traverse from bottom to top along the left column
-                if (left <= right) {
-                    for (int i = bottom; i >= top; i--) {
-                        res.add(matrix[i][left]);
-                    }
-                }
-                left++;  // Move the left boundary right
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+            if (left > right || top > bottom) {
+                break;
             }
 
-            return res;
+
+            for (int i = right; i >= left; i--) {
+                res.add(matrix[bottom][i]);
+            }
+            bottom--;
+            if (left > right || top > bottom) {
+                break;
+            }
+
+
+            for (int i = bottom; i >= top; i--) {
+                res.add(matrix[i][left]);
+            }
+            left++;
+            if (left > right || top > bottom) {
+                break;
+            }
         }
+
+        return res;
     }
 }
