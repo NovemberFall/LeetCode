a38998ff6a85ad2fc7b8c550a3752a47608a4501
TomZheng <1219.zheng@gmail.com>
2025-04-23 12:11:38 -0700

    lc 295

--- leetCode/src/main/java/Heap/_295_FindMedianFromDataStream.java
+++ leetCode/src/main/java/Heap/_295_FindMedianFromDataStream.java
@@ -3,34 +3,31 @@ package Heap;
 import java.util.PriorityQueue;
 
 class _295_FindMedianFromDataStream {
-    private final PriorityQueue<Integer> maxHeap;
-    private final PriorityQueue<Integer> minHeap;
+    private PriorityQueue<Integer> larger;
+    private PriorityQueue<Integer> smaller;
+    private boolean even = true;
 
     public _295_FindMedianFromDataStream() {
-        maxHeap = new PriorityQueue<>((a, b) -> b - a);
-        minHeap = new PriorityQueue<>((a, b) -> a - b);
+        larger = new PriorityQueue<>((a, b) -> b - a);
+        smaller = new PriorityQueue<>((a, b) -> a - b);
     }
 
     public void addNum(int num) {
-        if (maxHeap.isEmpty() || num < maxHeap.peek()) {
-            maxHeap.offer(num);
+        if (even) {
+            larger.offer(num);
+            smaller.offer(larger.poll());
         } else {
-            minHeap.offer(num);
+            smaller.offer(num);
+            larger.offer(smaller.poll());
         }
-
-        if (maxHeap.size() == minHeap.size() + 2) {
-            minHeap.offer(maxHeap.poll());
-        }
-        if (maxHeap.size() + 1 == minHeap.size()) {
-            maxHeap.offer(minHeap.poll());
-        }
+        even = !even;
     }
 
     public double findMedian() {
-        if (maxHeap.size() == minHeap.size()) {
-            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        if (even) {
+            return (smaller.peek() + larger.peek()) / 2.0;
         } else {
-            return maxHeap.peek();
+            return smaller.peek();
         }
     }
 }
