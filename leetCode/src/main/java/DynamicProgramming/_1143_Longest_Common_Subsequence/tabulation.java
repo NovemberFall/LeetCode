a01c6a2fc7a97993cb94c66d688a8fe4621e4001
NovemberFall <1219.zheng@gmail.com>
2025-04-26 22:44:00 -0700

    lc 10

--- leetCode/src/main/java/DynamicProgramming/_1143_Longest_Common_Subsequence/tabulation.java
+++ leetCode/src/main/java/DynamicProgramming/_1143_Longest_Common_Subsequence/tabulation.java
@@ -6,19 +6,19 @@ class tabulation {
 
         // dp[i][j] stores the length of the LCS of text1[0...i-1] and text2[0...j-1]
         int[][] dp = new int[m + 1][n + 1];
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (text1.charAt(i) == text2.charAt(j)) {
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
                     // If characters match, extend the LCS found so far
-                    dp[i][j] = dp[i + 1][j + 1] + 1;
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
                     // If characters don't match, take the maximum LCS from the previous subproblems
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
 
-        // The length of the LCS of the entire text1 and text2 is at dp[0][0]
-        return dp[0][0];
+        // The length of the LCS of the entire text1 and text2 is at dp[m][n]
+        return dp[m][n];
     }
 }
