e36e8495621d87b6a2f343f6d3974d7dc66afaeb
TomZheng <1219.zheng@gmail.com>
2025-05-11 13:36:59 -0700

    lc 128

--- leetCode/src/main/java/HashTable/_128_longestConsecutive/longestConsecutive.java
+++ leetCode/src/main/java/HashTable/_128_longestConsecutive/longestConsecutive.java
@@ -4,32 +4,24 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class longestConsecutive {
-    public static int longestConsecutive(int[] nums) {
+    public int longestConsecutive(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int n : nums) {
             set.add(n);
         }
         int longest = 0;
-        for (int n : nums) {
-            if (!set.contains(n - 1)) {
-                int len = 0;
-                while (set.contains(n + len)) {
+        for (int num : nums) {
+            if (!set.contains(num - 1)) {
+                int curNum = num;
+                int len = 1;
+                while (set.contains(curNum + 1)) {
+                    curNum += 1;
                     len += 1;
                 }
-                longest = Math.max(len, longest);
+                longest = Math.max(longest, len);
             }
 
         }
         return longest;
-    }
-
-    public static void main(String[] args) {
-        int[] nums = new int[]{0, 3, 7, 2, 5, 8, 4, 6, 0, 1};
-        int res = longestConsecutive(nums);
-        System.out.println(res);
-
-        int[] nums1 = new int[]{100,4,200,1,3,2};
-        res = longestConsecutive(nums1);
-        System.out.println(res);
     }
 }
